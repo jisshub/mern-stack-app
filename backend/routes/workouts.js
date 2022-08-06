77972701b6089034ab464c4e44aef84c493a95c2
jissmon jose <jissmon.jose@naicoits.com>
2022-08-06 13:51:02 +0530

    Create Diffrent routes

---
+++ backend/routes/workouts.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+// create routes instance
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.json({
+        "message": "welcome to workout app"
+    })
+}
+)
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
+require('dotenv').config();
 const expres = require('express');
+const workoutRoutes = require('./routes/workouts');
 
 const app = expres();
 
-app.listen(4000, () => {
+// middleware
+app.use((req, res, next)=>{
+    console.log(`${req.path}, ${req.method}`);
+    next();
+});
+
+app.use('/api/workouts', workoutRoutes);
+
+app.listen(process.env.PORT, () => {
     console.log('Server is running on port 4000');
-})
+});
